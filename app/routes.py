ec45039602b86a76f3d7c20224691b2a01d8cdaf
CaptainDaVinci <yashkothari199767@gmail.com>
2019-08-16 16:52:33 +0530

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,40 @@
+from flask import abort, request, jsonify
+from app import app
+import youtube_dl
+
+
+@app.route('/download/', methods=['GET'])
+def download():
+    yt_id = request.args.get('id')
+    encoding = request.args.get('format', 'mp3')
+    quality = request.args.get('quality', 'high')
+
+    app.logger.info('Request: {} {} {}'.format(yt_id, encoding, quality))
+
+    if yt_id == None:
+        abort(400)
+
+    if encoding not in ['mp3', 'mp4']:
+        abort(415)
+
+    ytdl_opts = dict()
+    if encoding == 'mp3':
+        ytdl_opts = app.config['MP3_OPTS']
+        if quality == 'low':
+            ytdl_opts['postprocessors'][0]['preferredquality'] = '124'
+    else:
+        ytdl_opts = app.config['MP4_OPTS']
+        if quality == 'low':
+            ytdl_opts['format'] = 'worst' 
+
+    try:
+        app.logger.info('Options {}'.format(ytdl_opts))
+        with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
+             ytdl.download([yt_id])
+        resp = {
+            'download_path': '{}{}/{}.{}'.format(request.url_root, app.config['STORAGE_DIR'], yt_id, encoding)
+        }
+        return jsonify(resp)
+    except Exception as e:
+        app.logger.critical(str(e))
+        abort(500)
---
+++ config.py
@@ -0,0 +1,51 @@
+import os
+import logging
+
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+logging.basicConfig(
+	filename='music-dl.log',
+	level=logging.DEBUG,
+	format='%(asctime)s %(levelname)s: %(message)s',
+        datefmt='%d-%m-%Y %H:%M:%S')
+
+
+class music_dl_log(object):
+    def debug(self, msg):
+        if 'ETA' not in msg:
+            logging.debug(msg)
+
+    def warning(self, msg):
+        logging.warning(msg)
+
+    def error(self, msg):
+        logging.error(msg)
+
+
+class Config(object):
+    SECRET_KEY = 'abcdefghijklmnopqrstuvwxyz'
+    STORAGE_DIR = 'media'
+    STORAGE_DIR_PATH = os.path.join(base_dir, STORAGE_DIR, '%(id)s.%(ext)s')
+
+    MP3_OPTS = {
+        'no_warnings': True,
+        'quiet': True,
+        'outtmpl': STORAGE_DIR_PATH,
+        'format': 'bestaudio/best',
+        'logger': music_dl_log(),
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'
+        }, {
+            'key': 'FFmpegMetadata'
+        }]
+    }
+
+    MP4_OPTS = {
+        'no_warnings': True,
+        'quiet': True,
+        'outtmpl': STORAGE_DIR_PATH,
+        'format': 'best',
+        'logger': music_dl_log()
+    }
